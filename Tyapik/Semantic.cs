cb72402ec99faa4d22d53e43f088757538aefae5
ManFizz <57350669+ManFizz@users.noreply.github.com>
2023-06-20 14:52:56 +1000

    add semantic check

--- Tyapik/Semantic.cs
+++ Tyapik/Semantic.cs
@@ -2,8 +2,24 @@
 
 public class Semantic
 {
-    public static void Check(Node tree)
+    public static void Check(Node tree, List<string>? variables = null)
     {
+        if (tree.childrens.Count == 0)
+            return;
         
+        variables = variables == null ? new List<string>() : new List<string>(variables);
+        foreach (var node in tree.childrens)
+        {
+            Check(node, variables);
+            if (node.pattern == Parser.MODIFICATION && !variables.Contains(node.childrens[0].value))
+                variables.Add(node.childrens[0].value); //add identifier
+
+            if (node.pattern == Parser.IDENTIFIER 
+                && !Lexem.KEYWORDS.ContainsKey(node.value) 
+                && !variables.Contains(node.value) 
+                && !int.TryParse(node.value, out _)
+                )
+                throw new Exception($"Semantic error: Undefined varable - \"{node.value}\"");
+        }
     }
 }
